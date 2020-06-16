2a4b48954e83cb01f791347db86570c4679b11ce
Marc Sibson <125162+sibson@users.noreply.github.com>
2020-06-16 08:26:34 -0400

    leveldb: add compare commands

--- leveldb.py
+++ leveldb.py
@@ -1,4 +1,4 @@
-import binascii, hashlib
+import binascii, hashlib, json
 
 import click
 import plyvel
@@ -6,6 +6,10 @@ import plyvel
 @click.group()
 def cli():
     pass
+
+
+def add_file(ldb, sha, filename):
+    ldb.put(sha.encode(), filename.encode())
 
 
 @cli.command()
@@ -21,11 +25,40 @@ def rm(dbpath):
 
 
 @cli.command()
-@click.option('--dbpath', default='.mop.db')
+@click.argument('dbpath')
 def ls(dbpath):
     ldb = plyvel.DB(dbpath)
     for sha, path in ldb:
         print(f'{sha.decode("UTF-8")} {path.decode("UTF-8")}')
+
+
+
+@cli.command()
+@click.argument('target')
+@click.argument('source', nargs=-1)
+def missing(target, sources):
+    target = plyvel.DB(target)
+    sources = [(s, plyvel.DB(s)) for s in sources]
+
+    for srcname, src  in sources:
+        for sha, path in src:
+            if target.get(sha):
+                continue
+
+            print(f'{srcname}\t{path.decode()}')
+
+
+@cli.command()
+@click.argument('target')
+@click.argument('source', nargs=-1)
+def present(target, source):
+    target = plyvel.DB(target)
+    sources = [(s, plyvel.DB(s)) for s in source]
+
+    for srcname, src  in sources:
+        for sha, path in src:
+            if target.get(sha):
+                print(f'{srcname}\t{path.decode()}')
 
 
 if __name__ == '__main__':
